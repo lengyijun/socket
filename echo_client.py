f67771fd04bc3021cf44017e61dae0fb45c38234
sjtu-lyj <sjtu5140809011@gmail.com>
2015-11-26 16:55:39 +0800

    both client and server have gui

--- echo_client.py
+++ echo_client.py
@@ -16,37 +16,39 @@ def get():
     ss=s.recv(1024)
     tv_get.insert(1.0,ss)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 建立连接:
-s.connect(('127.0.0.1', 9999))
-# 接收欢迎消息:
-print s.recv(1024)
+if __name__ == '__main__':
 
-# 以下为图形化编程
-root=Tk()
-root.title("echo_client")
-root.geometry('200x200')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 建立连接:
+    s.connect(('127.0.0.1', 9999))
+    # 接收欢迎消息:
+    print s.recv(1024)
 
-frm=Frame(root)
-frm_l=Frame(frm)
-frm_r=Frame(frm)
+    # 以下为图形化编程
+    root=Tk()
+    root.title("echo_client")
+    root.geometry('200x200')
 
-tv_send=Text(frm_r)
-tv_send.insert(1.0,"<<<<")
-tv_send.pack(side=TOP)
-t=tv_send.get(1.0,END)
-Button(frm_l,text="send",command=lambda :send(s,t)).pack(side=TOP)
-Button(frm_l,text="get",command=lambda :get()).pack(side=TOP)
+    frm=Frame(root)
+    frm_l=Frame(frm)
+    frm_r=Frame(frm)
 
-tv_get=Text(frm_r)
-tv_get.insert(1.0,"<<<<")
-tv_get.pack(side=TOP)
+    tv_send=Text(frm_r)
+    tv_send.insert(1.0,"<<<<")
+    tv_send.pack(side=TOP)
+    t=tv_send.get(1.0,END)
+    Button(frm_l,text="send",command=lambda :send(s,t)).pack(side=TOP)
+    Button(frm_l,text="get",command=lambda :get()).pack(side=TOP)
 
-frm_l.pack(side=LEFT)
-frm_r.pack(side=RIGHT)
-frm.pack()
-root.mainloop()
+    tv_get=Text(frm_r)
+    tv_get.insert(1.0,"<<<<")
+    tv_get.pack(side=TOP)
 
-s.close()
+    frm_l.pack(side=LEFT)
+    frm_r.pack(side=RIGHT)
+    frm.pack()
+    root.mainloop()
+
+    s.close()
 
 
--- echo_server.py
+++ echo_server.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
 # 写的极好，开一个echo_server可以监听多个client发送的信息
 'a server example which send hello to client.'
@@ -7,8 +7,22 @@
 import time, socket, threading
 from Tkinter import *
 
-# root=Tk()
-# root.title("echo server")
+def opensocket():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 监听端口:
+    s.bind(('127.0.0.1', 9999))
+    s.listen(5)
+    print 'Waiting for connection...'
+    while True:
+        # 接受一个新连接:
+        sock, addr = s.accept()
+        # 创建新线程来处理TCP连接:
+        t = threading.Thread(target=tcplink, args=(sock, addr))
+        t.start()
+
+def socket_thread():
+    t=threading.Thread(target=opensocket)
+    t.start()
 
 def tcplink(sock, addr):
     print 'Accept new connection from %s:%s...' % addr
@@ -19,20 +33,34 @@ def tcplink(sock, addr):
         if data == 'exit' or not data:
             print ("exit")
             break
-        # print (addr)
+        tv_get.insert(1.0,addr[0]+" "+str(addr[1])+" "+data)
         print (addr[0]+" "+str(addr[1])+" "+data)
         sock.send('Hello, %s!' % data)
     sock.close()
     print 'Connection from %s:%s closed.' % addr
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 监听端口:
-s.bind(('127.0.0.1', 9999))
-s.listen(5)
-print 'Waiting for connection...'
-while True:
-    # 接受一个新连接:
-    sock, addr = s.accept()
-    # 创建新线程来处理TCP连接:
-    t = threading.Thread(target=tcplink, args=(sock, addr))
-    t.start()
+if __name__ == '__main__':
+    root=Tk()
+    root.title("server<<<")
+    root.geometry('200x200')
+
+    frm=Frame(root)
+    frm_l=Frame(frm)
+    frm_r=Frame(frm)
+
+    Button(frm_r,text="start socket",command=lambda :socket_thread()).pack(side=TOP)
+    Button(frm_r,text="send message",command=lambda :socket_thread()).pack(side=TOP)
+
+    tv_get=Text(frm_l)
+    tv_get.insert(1.0,"init")
+    tv_get.pack(side=TOP)
+
+    tv_send=Text(frm_l)
+    tv_send.insert(1.0,"init")
+    tv_send.pack(side=TOP)
+
+    frm_r.pack(side=RIGHT)
+    frm_l.pack(side=LEFT)
+    frm.pack()
+
+    root.mainloop()
